651a55c3c68316e6de753ef0a3f5d60aaf933453
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2022-07-15 14:59:23 -0500

    56:00

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 
-import {Navbar} from './components';
+import {Navbar, HomePage, Exchanges, Cryptocurrencies, CryptoDetails, News} from './components';
 
 import './App.css';
 
@@ -13,10 +13,29 @@ const App = () => {
         <Navbar/>
       </div>
       <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path= "/" element={<HomePage/>}/>
+              <Route exact path= "/exchanges" element={<Exchanges/>}/>
+              <Route exact path= "/cryptocurrencies" element={<Cryptocurrencies/>}/>
+              <Route exact path= "/crypto/:coinId" element={<CryptoDetails/>}/>
+              <Route exact path= "/news" element={<News/>}/>
+            </Routes>
+          </div>
+        </Layout>
 
-      </div>
       <div className="footer">
-
+        <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+          Bitcoin Gratis <br/>
+          Todos los izquierdos reservados.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
       </div>
     </div>
   );
---
+++ src/services/CryptoApi.js
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '63a1130a5dmshf54da8c68556e7bp12cb9cjsnd5ee9eaf4e82',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl =  'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
